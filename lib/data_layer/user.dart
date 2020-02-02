25bcfc75ed7c6e5017c22289a1daf9c4ac09e988
nidaldh <nidal.98@outlook.com>
2020-02-02 15:21:23 +0200

    first commit with initialize user model

---
+++ lib/data_layer/user.dart
@@ -0,0 +1,31 @@
+class User {
+  String firstName;
+  String lastName;
+  String password;
+  int age;
+  int phoneNumber;
+  String email;
+  double weight;
+  double height;
+  String location;
+
+  User(this.firstName, this.lastName, this.password, this.age, this.phoneNumber,
+      this.email, this.weight, this.height, this.location);
+
+}
+
+///user model:
+//first name
+//last name
+//password
+//age
+//email
+//phone number
+//weight
+//Height
+//location
+//additional info:
+//1) IBM
+//2) HbA1c
+//3) list of weight
+//4) ID

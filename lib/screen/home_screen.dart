dfc1323ab2ca6a39321116a8807aadbdf6cc367a
nidaldh <nidal.98@outlook.com>
2020-02-10 00:07:26 +0200

    get massage to display it in the main screen. and start to store image in firebase storage and get url and get it from firestore

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,6 +9,7 @@ import 'package:demo_ican/screen/video_screen.dart';
 import 'package:demo_ican/screen/leacture_view.dart';
 import 'package:demo_ican/ui_layer/ibm/show_ibm.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -23,7 +24,7 @@ class HomeScreen extends StatelessWidget {
   User user;
   HomeScreen({Key key, @required this.email}) : super(key: key);
   Firestore firestore = Firestore.instance;
-
+  final StorageReference storageReference = FirebaseStorage().ref().child("");
   initUser() async {
     print("start");
     print("call firestore");
@@ -44,146 +45,156 @@ class HomeScreen extends StatelessWidget {
 //  print(user.name);
   }
 
+  Drawer sideBar(BuildContext context,var data){
+    return Drawer(
+      child: Container(
+        color: Colors.deepPurpleAccent,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+//                            return AddUser(
+//                              email: name,
+//                            );
+                        return UserProfile(user);
+                      }),
+                    );
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("user_profile"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  )),
+              Divider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        print(user);
+                        return ShowIBM(
+                          user: user,
+                        );
+                      }),
+                    );
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("add_BMI"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.insert_chart,
+                    color: Colors.white,
+                  )),
+              Divider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return VideoList();
+                      }),
+                    );
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("videos"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.video_label,
+                    color: Colors.white,
+                  )),
+              Divider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return LectureList();
+                      }),
+                    );
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("lecture"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.web,
+                    color: Colors.white,
+                  )),
+              Divider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              ListTile(
+                  onTap: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return RecipesList();
+                      }),
+                    );
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("Healthy_recipes"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.web,
+                    color: Colors.white,
+                  )),
+              Divider(
+                thickness: 1,
+                color: Colors.white,
+              ),
+              ListTile(
+                  onTap: () {
+                    if (data.savedLocale.toString().compareTo("ar_DZ") == 0)
+                      data.changeLocale(Locale('en', 'US'));
+                    else
+                      data.changeLocale(Locale('ar', 'DZ'));
+                  },
+                  title: Text(
+                    AppLocalizations.of(context).tr("clickMe"),
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  leading: Icon(
+                    Icons.language,
+                    color: Colors.white,
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  
+  
   @override
   Widget build(BuildContext context) {
+    print("1");
+
     initUser();
+    print("2");
+
+    getDate();print("3");
     var data = EasyLocalizationProvider.of(context).data;
     return EasyLocalizationProvider(
       data: data,
       child: Scaffold(
-        drawer: Drawer(
-          child: Container(
-            color: Colors.deepPurpleAccent,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ListView(
-                children: <Widget>[
-                  ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-//                            return AddUser(
-//                              email: name,
-//                            );
-                            return UserProfile(user);
-                          }),
-                        );
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("user_profile"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.person,
-                        color: Colors.white,
-                      )),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.white,
-                  ),
-                  ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            print(user);
-                            return ShowIBM(
-                              user: user,
-                            );
-                          }),
-                        );
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("add_BMI"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.insert_chart,
-                        color: Colors.white,
-                      )),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.white,
-                  ),
-                  ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            return VideoList();
-                          }),
-                        );
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("videos"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.video_label,
-                        color: Colors.white,
-                      )),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.white,
-                  ),
-                  ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            return LectureList();
-                          }),
-                        );
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("lecture"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.web,
-                        color: Colors.white,
-                      )),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.white,
-                  ),
-                  ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) {
-                            return RecipesList();
-                          }),
-                        );
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("Healthy_recipes"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.web,
-                        color: Colors.white,
-                      )),
-                  Divider(
-                    thickness: 1,
-                    color: Colors.white,
-                  ),
-                  ListTile(
-                      onTap: () {
-                        if (data.savedLocale.toString().compareTo("ar_DZ") == 0)
-                          data.changeLocale(Locale('en', 'US'));
-                        else
-                          data.changeLocale(Locale('ar', 'DZ'));
-                      },
-                      title: Text(
-                        AppLocalizations.of(context).tr("clickMe"),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      leading: Icon(
-                        Icons.language,
-                        color: Colors.white,
-                      )),
-                ],
-              ),
-            ),
-          ),
-        ),
+        drawer: sideBar(context, data),
         appBar: AppBar(
           backgroundColor: Colors.deepPurple,
           title:
@@ -200,14 +211,94 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-//            Center(
-//                child: Text(AppLocalizations.of(context)
-//                    .tr("msg", args: [email, 'flutter']))),
-          ],
+            FutureBuilder(
+              future: getNotify(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState ==
+                    ConnectionState.waiting)
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      child: Card(child: Center(child: CircularProgressIndicator()),),
+                    ),
+                  );
+
+                if (snapshot.connectionState ==
+                    ConnectionState.done)
+                  return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+
+                    itemCount: snapshot.data.length,
+                      itemBuilder: (_,index){
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                              color: Colors.amber,
+                              child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Center(child: Text(snapshot.data[index].data['note'])),
+                          )),
+                        );
+                  });
+                return Container();
+              },
+            ),
+            FutureBuilder(
+              future: getImage(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState ==
+                    ConnectionState.waiting)
+                  return Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Container(
+                      child: Card(child: Center(child: CircularProgressIndicator()),),
+                    ),
+                  );
+
+                if (snapshot.connectionState ==
+                    ConnectionState.done)
+                  return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (_,index){
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(child:Image.network(snapshot.data[index].data['url'])),
+                        );
+                      });
+                return Container();
+              },
+            )          ],
         ),
+
       ),
     );
+  }
+
+
+  getDate() async {
+    print("in");
+    QuerySnapshot querySnapshot = await firestore
+        .collection("notify")
+        .getDocuments();
+    querySnapshot.documents.forEach((f) {
+      print("add");
+      print(f.documentID);
+    });
+  }
+
+  Future getNotify() async{
+    QuerySnapshot qn=  await firestore.collection("notify").getDocuments();
+    return qn.documents;
+  }
+
+  Future getImage() async{
+    QuerySnapshot qn=  await firestore.collection("image").getDocuments();
+    return qn.documents;
   }
 }

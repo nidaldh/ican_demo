0c9e954694b75a93371f974ae8edb746f1d5a547
nidaldh <nidal.98@outlook.com>
2020-02-11 21:20:02 +0200

    change the color of the chat add fix the problems

--- lib/ui_layer/chat/chat_screen.dart
+++ lib/ui_layer/chat/chat_screen.dart
@@ -1,18 +1,22 @@
-import 'dart:io';
-import 'dart:math';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demo_ican/data_layer/model/user.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firestore_ui/animated_firestore_list.dart';
+import 'package:intl/intl.dart';
+import 'package:progress_dialog/progress_dialog.dart';
 import 'dart:async';
 
 import 'message2.dart';
 
 class ChatScreen2 extends StatefulWidget {
+  User user;
+
+  ChatScreen2({this.user});
+
   @override
   State createState() => new ChatScreenState();
 }
@@ -28,35 +32,31 @@ class ChatScreenState extends State<ChatScreen2> {
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
-          title: new Text("Friendlychat"),
+          backgroundColor: Colors.purple,
+          title: new Text("Group chat"),
           elevation:
               Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
         ),
         body: new Column(children: <Widget>[
           new Flexible(
-            child: new FirestoreAnimatedList(
-              query: query,
-//              sort: (a, b) => b.key.compareTo(a.key),
-//              padding: new EdgeInsets.all(8.0),
-//              reverse: true,
-//              itemBuilder: (_, DocumentSnapshot snapshot) {//, Animation<double> animation
-//                return new ChatMessage(
-//                    snapshot: snapshot,
-////                    animation: animation
-//                );
-//              },
-              itemBuilder: (
-                BuildContext context,
-                DocumentSnapshot snapshot,
-                Animation<double> animation,
-                int index,
-              ) =>
-                  FadeTransition(
-                opacity: animation,
-                child: ChatMessage(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(1, 3, 1, 0),
+              child: new FirestoreAnimatedList(
+                query: query,
+                itemBuilder: (
+                  BuildContext context,
+                  DocumentSnapshot snapshot,
+                  Animation<double> animation,
+                  int index,
+                ) =>
+                    FadeTransition(
+                  opacity: animation,
+                  child: ChatMessage(
 //                  index: index,
-                  snapshot: snapshot,
+                    snapshot: snapshot,
+                    me: snapshot.data['senderName'] == widget.user.name,
 //                  onTap: _removeMessage,
+                  ),
                 ),
               ),
             ),
@@ -69,10 +69,29 @@ class ChatScreenState extends State<ChatScreen2> {
         ]));
   }
 
+  ProgressDialog pr;
+
   Widget _buildTextComposer() {
+    pr = new ProgressDialog(context, type: ProgressDialogType.Normal);
+
+//    pr.style(message: 'Showing some progress...');
+
+    pr.style(
+      message: 'Send Image',
+      borderRadius: 10.0,
+      backgroundColor: Colors.white,
+      progressWidget: CircularProgressIndicator(),
+      elevation: 10.0,
+      insetAnimCurve: Curves.easeInOut,
+      progressTextStyle: TextStyle(
+          color: Colors.black, fontSize: 13.0, fontWeight: FontWeight.w400),
+      messageTextStyle: TextStyle(
+          color: Colors.black, fontSize: 19.0, fontWeight: FontWeight.w600),
+    );
+
     return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-      child: new Container(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
           margin: const EdgeInsets.symmetric(horizontal: 8.0),
           child: new Row(children: <Widget>[
             new Container(
@@ -80,30 +99,19 @@ class ChatScreenState extends State<ChatScreen2> {
               child: new IconButton(
                   icon: new Icon(Icons.photo_camera),
                   onPressed: () async {
+                    pr.show();
                     print("images pikecr");
 //                    File imageFile = await ImagePicker.pickImage();
-                    var image =
-                        await ImagePicker.pickImage(source: ImageSource.gallery);
-                    int random = new Random().nextInt(100000);
-//                    StorageReference ref =
-//                    FirebaseStorage.instance.ref().child("image_$random.jpg");
-//                    FirebaseStorage _storage =
-//                    FirebaseStorage(storageBucket: 'gs://icanhel-demo.appspot.com/');
-//                    String filePath = 'images/${DateTime.now()}.png';
-//                    _storage.ref().child(filePath).putFile(image);
-//                    StorageUploadTask uploadTask = ref.putFile(image);
-//                    StorageTaskSnapshot storageTaskSnapshot = await uploadTask.onComplete;
-//                    String downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
-//                    Uri downloadUrl = (await uploadTask.onComplete).downloadUrl();
-
+                    var image = await ImagePicker.pickImage(
+                        source: ImageSource.gallery);
                     final FirebaseStorage _storage = FirebaseStorage(
                         storageBucket: 'gs://icanhel-demo.appspot.com/');
-
                     StorageUploadTask _uploadTask;
                     String filePath = 'images/${DateTime.now()}.png';
                     _uploadTask = _storage.ref().child(filePath).putFile(image);
                     StorageTaskSnapshot storageTaskSnapshot =
                         await _uploadTask.onComplete;
+                    pr.hide();
                     String downloadUrl =
                         await storageTaskSnapshot.ref.getDownloadURL();
                     _sendMessage(imageUrl: downloadUrl);
@@ -124,26 +132,14 @@ class ChatScreenState extends State<ChatScreen2> {
             ),
             new Container(
                 margin: new EdgeInsets.symmetric(horizontal: 4.0),
-                child: Theme.of(context).platform == TargetPlatform.iOS
-                    ? new CupertinoButton(
-                        child: new Text("Send"),
-                        onPressed: _isComposing
-                            ? () => _handleSubmitted(_textController.text)
-                            : null,
-                      )
-                    : new IconButton(
-                        icon: new Icon(Icons.send),
-                        onPressed: _isComposing
-                            ? () => _handleSubmitted(_textController.text)
-                            : null,
-                      )),
+                child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: _isComposing
+                      ? () => _handleSubmitted(_textController.text)
+                      : null,
+                )),
           ]),
-          decoration: Theme.of(context).platform == TargetPlatform.iOS
-              ? new BoxDecoration(
-                  border:
-                      new Border(top: new BorderSide(color: Colors.grey[200])))
-              : null),
-    );
+        ));
   }
 
   Future<Null> _handleSubmitted(String text) async {
@@ -155,11 +151,21 @@ class ChatScreenState extends State<ChatScreen2> {
   }
 
   void _sendMessage({String text, String imageUrl}) {
-    reference.document().setData({
+    print(DateTime.now().millisecondsSinceEpoch);
+    var format = DateFormat.yMd();
+    var format2 = DateFormat('kk:mm:ss  y-M-d', 'ar');
+    var dateString = format.format(DateTime.now());
+    var dateString2 = format2.format(DateTime.now());
+    print(dateString);
+    print(dateString2);
+
+    reference
+        .document(DateTime.now().millisecondsSinceEpoch.toString())
+        .setData({
       'text': text,
       'imageUrl': imageUrl,
-      'senderName': "nidal@gmail.com",
-//      'senderPhotoUrl': googleSignIn.currentUser.photoUrl,
+      'date': dateString2,
+      'senderName': widget.user.name,
     });
     analytics.logEvent(name: 'send_message');
   }
--- lib/ui_layer/chat/message2.dart
+++ lib/ui_layer/chat/message2.dart
@@ -1,16 +1,17 @@
+import 'package:bubble/bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 @override
 class ChatMessage extends StatelessWidget {
-  ChatMessage({this.snapshot, this.animation});
+  ChatMessage({this.snapshot, this.animation, this.me});
   final DocumentSnapshot snapshot;
   final Animation animation;
+  bool me;
 
   Widget build(BuildContext context) {
-
     return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      margin: const EdgeInsets.symmetric(vertical: 2.0, horizontal: 5),
       child: new Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
@@ -18,24 +19,43 @@ class ChatMessage extends StatelessWidget {
 //              margin: const EdgeInsets.only(right: 16.0),
 //              child: new CircleAvatar(backgroundImage: new NetworkImage(snapshot.data['senderPhotoUrl'])),
 //            ),
-          new Expanded(
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                new Text(
-                    snapshot.data['senderName'],
-                    style: Theme.of(context).textTheme.subhead),
-                new Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child:
-                    snapshot.data['imageUrl'] != null ?
-                    new Image.network(
-                      snapshot.data['imageUrl'],
-                      width: 250.0,
-                    ) :
-                  new Text(snapshot.data['text']),
+          Expanded(
+            child: Tooltip(
+              child: Bubble(
+                color: me ? Colors.amberAccent : Colors.deepPurpleAccent,
+//              borderRadius: BorderRadius.circular(10.0),
+                nip: me ? BubbleNip.rightBottom : BubbleNip.leftBottom,
+                alignment: me ? Alignment.topRight : Alignment.topLeft,
+                elevation: 6.0,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment:
+                        me ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+//                  crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      if (!me)
+                        new Text(snapshot.data['senderName'],
+                            style: Theme.of(context).textTheme.subhead),
+                      new Container(
+                        margin: const EdgeInsets.only(top: 5),
+                        child: snapshot.data['imageUrl'] != null
+                            ? Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: new Image.network(
+                                  snapshot.data['imageUrl'],
+                                  width: 250.0,
+                                ),
+                              )
+                            : new Text(snapshot.data['text']),
+                      ),
+                    ],
+                  ),
                 ),
-              ],
+              ),
+              message:snapshot.data['date'] ,
             ),
           ),
         ],
@@ -43,5 +63,3 @@ class ChatMessage extends StatelessWidget {
     );
   }
 }
-
-

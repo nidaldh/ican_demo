5e89fc467d02ec84a944def70f1d6d2bb059a1f4
nidaldh <nidal.98@outlook.com>
2020-02-29 00:53:27 +0200

    after select account display bottom sheet with two option user profile and IBM

--- lib/ui_layer/admin/admin_screen.dart
+++ lib/ui_layer/admin/admin_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:demo_ican/data_layer/model/user.dart';
 import 'package:demo_ican/screen/user_profile.dart';
+import 'package:demo_ican/ui_layer/ibm/show_ibm.dart';
 import 'package:firestore_ui/firestore_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,7 +20,13 @@ class _AdminScreenState extends State<AdminScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text(
+          "المستخدمين",
+          style: GoogleFonts.cairo(
+              color: Colors.white, fontWeight: FontWeight.w700, fontSize: 20),
+        ),
+      ),
       body: FirestoreAnimatedList(
         errorChild: Card(
             color: Colors.amber,
@@ -27,7 +34,7 @@ class _AdminScreenState extends State<AdminScreen> {
               padding: const EdgeInsets.all(20.0),
               child: Center(
                   child: Text(
-                "no notification",
+                "no accounts",
                 style: GoogleFonts.cairo(
                     color: Colors.white,
                     fontWeight: FontWeight.w700,
@@ -48,24 +55,7 @@ class _AdminScreenState extends State<AdminScreen> {
           opacity: animation,
           child: InkWell(
             onTap: () {
-              User user;
-              try{
-               user = User(
-                  snapshot.data['name'],
-                  snapshot.data['age'],
-                  snapshot.data['phone_number'],
-                  snapshot.data['weight'],
-                  snapshot.data['height'],
-                  snapshot.data['location'],email: snapshot.documentID,);}
-                  catch (e){
-                print(e);
-                  }
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (context) {
-                  return UserProfile(user);
-//                        return PopImage();
-                }),
-              );
+              _settingModalBottomSheet(context, snapshot);
             },
             child: Card(
                 color: Colors.amber,
@@ -81,11 +71,73 @@ class _AdminScreenState extends State<AdminScreen> {
                   )),
                 )),
           ),
-//                        Text(
-//                           snapshot.data['note'],
-//                        ),
         ),
       ),
     );
+  }
+
+  void _settingModalBottomSheet(context, DocumentSnapshot snapshot) {
+    User user;
+    try {
+      user = User(
+        snapshot.data['name'],
+        snapshot.data['age'],
+        snapshot.data['phone_number'],
+        snapshot.data['weight'],
+        snapshot.data['height'],
+        snapshot.data['location'],
+        email: snapshot.documentID,
+      );
+    } catch (e) {
+      print(e);
+    }
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            child: new Wrap(
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(user.name,
+                        style: GoogleFonts.cairo(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20)),
+                  ],
+                ),
+                Divider(
+                  height: 1,
+                  color: Colors.black12,
+                  thickness: 1,
+                ),
+                new ListTile(
+                    leading: new Icon(Icons.person),
+                    title: new Text('profile'),
+                    onTap: () => {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) {
+                              return UserProfile(user);
+                            }),
+                          )
+                        }),
+                new ListTile(
+                  leading: new Icon(Icons.insert_chart),
+                  title: new Text('IBM'),
+                  onTap: () => {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) {
+                        return ShowIBM(
+                          user: user,
+                        );
+                      }),
+                    )
+                  },
+                ),
+              ],
+            ),
+          );
+        });
   }
 }

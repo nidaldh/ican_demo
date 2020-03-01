b274c81f1081338ef0ab859a879fefc52adeceab
nidaldh <nidal.98@outlook.com>
2020-03-02 00:02:54 +0200

    push notification from the admin

--- lib/ui_layer/admin/note.dart
+++ lib/ui_layer/admin/note.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:demo_ican/data_layer/send_notification/messaging.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,10 +14,59 @@ class NoteScreen extends StatefulWidget {
 }
 
 class _NoteScreenState extends State<NoteScreen> {
+  final FirebaseMessaging firebaseMessaging = new FirebaseMessaging();
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  new FlutterLocalNotificationsPlugin();
   int index;
   final _formKey = GlobalKey<FormState>();
   final Firestore _firestore = Firestore.instance;
   String note="";
+  String title="";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    registerNotification();
+    super.initState();
+  }
+  void sendTokenToServer(String fcmToken) {
+    print('token: $fcmToken');
+  }
+
+  void sendNotification(String title,String body) async {
+    await Messaging.sendToAll(title: title, body: body);
+  }
+
+  void registerNotification() {
+
+    firebaseMessaging.onTokenRefresh.listen(sendTokenToServer);
+    firebaseMessaging.getToken();
+    firebaseMessaging.subscribeToTopic('all');
+    print("in initial");
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        final notification = message['notification'];
+        setState(() {
+          print("hal hal hal:   "+ notification['body']);
+          print("onMessage: $message");
+        });
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        setState(() {
+          print("onLaunch: $message");
+        });
+
+        // TODO optional
+      },
+      onResume: (Map<String, dynamic> message) async {
+        setState(() {
+          print("onResume: $message");
+        });
+
+        // TODO optional
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +81,31 @@ class _NoteScreenState extends State<NoteScreen> {
               child: TextFormField(
                 initialValue: note,
                 decoration: InputDecoration(
-                  icon: Icon(Icons.search),
+                  icon: Icon(Icons.title),
+                  labelText: AppLocalizations.of(context).tr("note_title"),
+                  border: new OutlineInputBorder(
+                      borderSide:
+                      new BorderSide(color: Colors.amberAccent)),
+                ),
+                validator: (value) {
+                  if (value.trim().isEmpty) {
+                    return AppLocalizations.of(context).tr("empty_input");
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  print(value);
+                  title = value.trim();
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                initialValue: note,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.note),
                   labelText: AppLocalizations.of(context).tr("note"),
-                  hintText:
-                  AppLocalizations.of(context).tr("note_hint"),
                   border: new OutlineInputBorder(
                       borderSide:
                       new BorderSide(color: Colors.amberAccent)),
@@ -59,7 +132,6 @@ class _NoteScreenState extends State<NoteScreen> {
                         AppLocalizations.of(context).tr("push"),
                         style: GoogleFonts.cairo(color: Colors.white),
                       ),
-//                            Icon(Icons.person)
                     ],
                   ),
                   color: Colors.deepPurple,
@@ -91,5 +163,6 @@ class _NoteScreenState extends State<NoteScreen> {
       Fluttertoast.showToast(msg: "error");
     });
     Fluttertoast.showToast(msg: "updated");
+    sendNotification( title, note);
   }
 }
